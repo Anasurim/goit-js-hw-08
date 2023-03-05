cd7f696337599ba5a8882d1a4793e87035a93062
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-03-05 15:41:56 +0100

    js-hw-08 task2 vimeoPlayerTask  completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,26 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+function onTimeupdate() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      // seconds = the current playback position
+      savedCurrentTime(seconds);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function savedCurrentTime(seconds) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+}
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(savedTime);
